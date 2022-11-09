55e0522ab9fba1636c1fc4bf695da49c09b48bbf
Md Ali <mdali91935@gmail.com>
2022-11-09 21:42:15 +0530

    added desktop layout

--- team_robin/src/App.js
+++ team_robin/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
     {/* write code  */}
-    <div>Team Robin</div>
+    {/* <div>Team Robin</div> */}
+    <Navbar />
     </div>
   );
 }
