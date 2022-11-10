a8ac7747f1831136d51e927bb28a4a2cc736db93
ashish bajaj <abajaj655@gmail.com>
2022-11-10 11:14:37 +0530

    code asish

--- team_robin/src/App.js
+++ team_robin/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BannerMen } from './components/BannerMen';
 import CategoriesMen from './components/CategoriesMen';
 import DiscountSection from './components/DiscountSectionMen';
+import Navbar from "./Navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
     {/* write code  */}
     {/* <div>Team Robin</div> */}
+    <Navbar />
     <BannerMen />
     <CategoriesMen />
     <DiscountSection />
