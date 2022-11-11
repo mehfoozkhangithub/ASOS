2a19199f287f8cf05c7897f98d9ffcb143eaa86c
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2022-11-11 12:13:04 +0530

    code change by mehfooz

--- team_robin/src/App.js
+++ team_robin/src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import { BannerMen } from './components/BannerMen';
-import BrandsSectionMen from './components/BrandsSectionMen';
-import CategoriesMen from './components/CategoriesMen';
-import CtaSectionMen from './components/CtaSectionMen';
-import DiscountSection from './components/DiscountSectionMen';
-import Navbar from "./Navbar/Navbar"
-import { ProductPage } from './ProductPage/ProductPage';
-import MensClothing from './Productspage/MensClothing';
-import Shoes from './Productspage/Shoes';
-import { WomensClothing } from './Productspage/WomensClothing';
+import "./App.css";
+import { BannerMen } from "./components/BannerMen";
+import BrandsSectionMen from "./components/BrandsSectionMen";
+import CategoriesMen from "./components/CategoriesMen";
+import CtaSectionMen from "./components/CtaSectionMen";
+import DiscountSection from "./components/DiscountSectionMen";
+import Navbar from "./Navbar/Navbar";
+import { ProductPage } from "./ProductPage/ProductPage";
+import MensClothing from "./Productspage/MensClothing";
+import Shoes from "./Productspage/Shoes";
+import { WomensClothing } from "./Productspage/WomensClothing";
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
-     <MensClothing/>
-     <Shoes/>
-     <WomensClothing/>
-  <ProductPage/>
-    <BannerMen />
-    <CategoriesMen />
-    <DiscountSection />
-    <CtaSectionMen />
-    <BrandsSectionMen />
+      <Navbar />
+      <MensClothing />
+      <Shoes />
+      <WomensClothing />
+      <ProductPage />
+      <BannerMen />
+      <CategoriesMen />
+      <DiscountSection />
+      <CtaSectionMen />
+      <BrandsSectionMen />
     </div>
   );
 }
-// hello every one 
+// hello every one
 
 export default App;
