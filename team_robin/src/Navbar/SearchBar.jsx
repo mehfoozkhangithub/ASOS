a40f5ea60194d9ca8446f2ae57053ebd108d824f
Md Ali <mdali91935@gmail.com>
2022-11-14 19:34:51 +0530

    added asos

--- team_robin/src/Navbar/SearchBar.jsx
+++ team_robin/src/Navbar/SearchBar.jsx
@@ -14,7 +14,7 @@ const Searchbar = () => {
                     setSearchData(contacts)
                     console.log(SearchData);
                 })
-        }, [SearchData,search])   
+        }, [])   
 
 
         const onSubmit=()=>{
