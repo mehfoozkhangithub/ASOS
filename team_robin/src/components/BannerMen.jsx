294693cb1bc47447a4e7e044ff01631c30ceca82
Md Ali <mdali91935@gmail.com>
2022-11-16 15:44:47 +0530

    added banner width

--- team_robin/src/components/BannerMen.jsx
+++ team_robin/src/components/BannerMen.jsx
@@ -5,7 +5,7 @@ function BannerMen(){
     return (
         <>
             <div>
-                <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/artworked-promos/high-awareness-markets/30/row/2022_singles-day_high-awareness_30-homepage-promo-hero-desktop_mw_row_1258x600.jpg" alt="" />
+                <img width="100%" src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/artworked-promos/high-awareness-markets/30/row/2022_singles-day_high-awareness_30-homepage-promo-hero-desktop_mw_row_1258x600.jpg" alt="" />
             </div>
         </>
         
